16895e41a16b1c19726f5b791a8844ec5b2482bf
AdarshTiwari79 <jaunpuradarsh62@gmail.com>
2024-07-11 12:24:56 +0530

    added a put controller to handle update request

--- src/main/java/com/springboot/restapi/restapi/controllers/MainRestController.java
+++ src/main/java/com/springboot/restapi/restapi/controllers/MainRestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,4 +43,12 @@ public class MainRestController {
   public void deleteBook(@PathVariable("id") int bookId) {
     this.restService.deleteBook(bookId);
   }
+
+  @PutMapping("book/{id}")
+  public Book updateBook(@PathVariable("id") int id, @RequestBody Book book) {
+
+    Book updatedBook = this.restService.updateBook(id, book);
+    return updatedBook;
+  }
+
 }
