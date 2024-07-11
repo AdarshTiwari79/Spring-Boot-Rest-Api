7016f328989bbd204dbe817916f32214f4a0b3f5
AdarshTiwari79 <jaunpuradarsh62@gmail.com>
2024-07-11 15:37:29 +0530

    using ResponseEntity class to send httpStatus code

--- src/main/java/com/springboot/restapi/restapi/controllers/MainRestController.java
+++ src/main/java/com/springboot/restapi/restapi/controllers/MainRestController.java
@@ -1,8 +1,11 @@
 package com.springboot.restapi.restapi.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,34 +24,58 @@ public class MainRestController {
   private RestService restService;
 
   @GetMapping("/books")
-  public List<Book> getAll() {
+  public ResponseEntity<List<Book>> getAll() {
     List<Book> books = this.restService.getAllBooks();
-    return books;
+    if (books.size() <= 0) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+    return ResponseEntity.of(Optional.of(books));
   }
 
   @GetMapping("/book/{id}")
-  public Book getSingleBook(@PathVariable("id") int bookId) {
+  public ResponseEntity<Book> getSingleBook(@PathVariable("id") int bookId) {
 
     Book book = restService.getBookById(bookId);
-    return book;
+    if (book == null) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+    return ResponseEntity.status(HttpStatus.OK).body(book);
   }
 
   @PostMapping("/book")
-  public Book addBook(@RequestBody Book b) {
-    Book book = this.restService.addBooks(b);
-    return book;
+  public ResponseEntity<Book> addBook(@RequestBody Book b) {
+    Book book;
+    try {
+      book = this.restService.addBooks(b);
+      return ResponseEntity.status(HttpStatus.CREATED).body(book);
+    } catch (Exception e) {
+      System.out.println(e);
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
   }
 
   @DeleteMapping("/book/{id}")
-  public void deleteBook(@PathVariable("id") int bookId) {
-    this.restService.deleteBook(bookId);
+  public ResponseEntity<Void> deleteBook(@PathVariable("id") int bookId) {
+    try {
+      this.restService.deleteBook(bookId);
+      return ResponseEntity.status(HttpStatus.OK).build();
+    } catch (Exception e) {
+      System.out.println(e);
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
   }
 
   @PutMapping("book/{id}")
-  public Book updateBook(@PathVariable("id") int id, @RequestBody Book book) {
+  public ResponseEntity<Book> updateBook(@PathVariable("id") int id, @RequestBody Book book) {
 
-    Book updatedBook = this.restService.updateBook(id, book);
-    return updatedBook;
+    Book updatedBook;
+    try {
+      updatedBook = this.restService.updateBook(id, book);
+      return ResponseEntity.status(HttpStatus.OK).body(updatedBook);
+    } catch (Exception e) {
+      System.out.println(e);
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
   }
 
 }
