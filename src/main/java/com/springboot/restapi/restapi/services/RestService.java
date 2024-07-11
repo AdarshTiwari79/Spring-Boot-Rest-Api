b8950e7a16a61b04777e12d723572c295dd481cb
AdarshTiwari79 <jaunpuradarsh62@gmail.com>
2024-07-11 11:01:24 +0530

    added a method to perform delete operation using stream api

--- src/main/java/com/springboot/restapi/restapi/services/RestService.java
+++ src/main/java/com/springboot/restapi/restapi/services/RestService.java
@@ -2,6 +2,7 @@ package com.springboot.restapi.restapi.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -11,26 +12,29 @@ import com.springboot.restapi.restapi.entities.Book;
 public class RestService {
 
   static final List<Book> books = new ArrayList<>();
-  static{
-    books.add(new Book(1,"Learn java","java","Ganesh"));
-    books.add(new Book(2,"Spring Boot","Spring","Adarsh"));
-    books.add(new Book(3,"JSTL","jsp","Saurav"));
+  static {
+    books.add(new Book(1, "Learn java", "java", "Ganesh"));
+    books.add(new Book(2, "Spring Boot", "Spring", "Adarsh"));
+    books.add(new Book(3, "JSTL", "jsp", "Saurav"));
   }
 
-  public List<Book> getAllBooks(){
+  public List<Book> getAllBooks() {
     return books;
   }
 
-  public Book getBookById(int id){
-    
-    Book bookById = books.stream().filter(book->book.getId()==id).findFirst().get();
+  public Book getBookById(int id) {
+
+    Book bookById = books.stream().filter(book -> book.getId() == id).findFirst().get();
     return bookById;
   }
 
-  public Book addBooks(Book book)
-  {
+  public Book addBooks(Book book) {
     books.add(book);
     return book;
+  }
+
+  public void deleteBook(int bId) {
+    books.stream().filter(book -> book.getId() != bId).collect(Collectors.toList());
   }
 
 }
