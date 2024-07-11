76f299ed6dc3220b99065f4902e19e8194063bec
AdarshTiwari79 <jaunpuradarsh62@gmail.com>
2024-07-11 11:04:36 +0530

    added a delete controller to handle delete request

--- src/main/java/com/springboot/restapi/restapi/controllers/MainRestController.java
+++ src/main/java/com/springboot/restapi/restapi/controllers/MainRestController.java
@@ -3,6 +3,7 @@ package com.springboot.restapi.restapi.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,36 +13,33 @@ import org.springframework.web.bind.annotation.RestController;
 import com.springboot.restapi.restapi.entities.Book;
 import com.springboot.restapi.restapi.services.RestService;
 
-
-
-
 @RestController
 public class MainRestController {
-  
+
   @Autowired
   private RestService restService;
 
-  @GetMapping("/books") 
-  public List<Book> getAll()
-  {
+  @GetMapping("/books")
+  public List<Book> getAll() {
     List<Book> books = this.restService.getAllBooks();
     return books;
   }
 
   @GetMapping("/book/{id}")
   public Book getSingleBook(@PathVariable("id") int bookId) {
-      
-    Book book = restService.getBookById(bookId);  
+
+    Book book = restService.getBookById(bookId);
     return book;
   }
 
   @PostMapping("/book")
-  public Book addBook(@RequestBody Book b)
-  {
+  public Book addBook(@RequestBody Book b) {
     Book book = this.restService.addBooks(b);
     return book;
   }
-  
-  
-  
+
+  @DeleteMapping("/book/{id}")
+  public void deleteBook(@PathVariable("id") int bookId) {
+    this.restService.deleteBook(bookId);
+  }
 }
