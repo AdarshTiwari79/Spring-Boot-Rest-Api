564eff191a01e1f2c79a68912f86f55602721cb3
AdarshTiwari79 <jaunpuradarsh62@gmail.com>
2024-07-13 09:31:25 +0530

    using @JsonBackReference with @OneToOne to achieve bidirectional mapping on Book entity property

--- src/main/java/com/springboot/restapi/restapi/entities/Author.java
+++ src/main/java/com/springboot/restapi/restapi/entities/Author.java
@@ -1,9 +1,12 @@
 package com.springboot.restapi.restapi.entities;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Author {
@@ -15,14 +18,19 @@ public class Author {
   private String lastName;
   private String language;
 
+  @JsonBackReference
+  @OneToOne(mappedBy = "author")
+  private Book book;
+
   public Author() {
   }
 
-  public Author(int author_Id, String firstName, String lastName, String language) {
+  public Author(int author_Id, String firstName, String lastName, String language, Book book) {
     this.author_Id = author_Id;
     this.firstName = firstName;
     this.lastName = lastName;
     this.language = language;
+    this.book = book;
   }
 
   public int getAuthor_Id() {
@@ -55,6 +63,14 @@ public class Author {
 
   public void setLanguage(String language) {
     this.language = language;
+  }
+
+  public Book getBook() {
+    return book;
+  }
+
+  public void setBook(Book book) {
+    this.book = book;
   }
 
 }
