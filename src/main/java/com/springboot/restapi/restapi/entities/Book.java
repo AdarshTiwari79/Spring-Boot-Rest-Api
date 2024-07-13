23c3b4b886076486399d45b5c32ccfcc8f3d6120
AdarshTiwari79 <jaunpuradarsh62@gmail.com>
2024-07-13 08:47:15 +0530

    using a entity type variable with oneToOne mapping

--- src/main/java/com/springboot/restapi/restapi/entities/Book.java
+++ src/main/java/com/springboot/restapi/restapi/entities/Book.java
@@ -1,9 +1,11 @@
 package com.springboot.restapi.restapi.entities;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Book {
@@ -12,16 +14,26 @@ public class Book {
   private int id;
   private String book_title;
   private String book_name;
-  private String book_author;
+
+  @OneToOne(cascade = CascadeType.ALL)
+  private Author author;
 
   public Book() {
   }
 
-  public Book(int id, String book_title, String book_name, String book_author) {
-    this.id = id;
-    this.book_title = book_title;
+  public Book(Author author, String book_name, String book_title, int id) {
+    this.author = author;
     this.book_name = book_name;
-    this.book_author = book_author;
+    this.book_title = book_title;
+    this.id = id;
+  }
+
+  public Author getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(Author author) {
+    this.author = author;
   }
 
   public void setId(int id) {
@@ -36,10 +48,6 @@ public class Book {
     this.book_name = book_name;
   }
 
-  public void setBook_author(String book_author) {
-    this.book_author = book_author;
-  }
-
   public int getId() {
     return id;
   }
@@ -50,10 +58,6 @@ public class Book {
 
   public String getBook_name() {
     return book_name;
-  }
-
-  public String getBook_author() {
-    return book_author;
   }
 
 }
